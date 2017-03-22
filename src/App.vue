b81d337ad148f0a5d948f2162a69849f7c075363
dragon <dragonzhu1988@126.com>
2017-03-22 15:26:46 +0700

    lanuch

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,35 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
-  </div>
+    <div id="app">
+        {{ msg }}
+
+        <vcomponent></vcomponent>
+    </div>
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+    //import component
+    import Vcomponent from "./components/vcomponent/vcomponent.vue"
+    export default {
+      name: 'app',
+      data(){
+        return {
+          msg : 'hello'
+        }
+      },
+    //add component
+      components:{
+         Vcomponent
+      }
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
